4c35264635155963a7a93764900853c99e7961d7
jeferson <jefersongomes333@hotmail.com>
2020-08-14 14:36:57 -0300

    Create Palavra.java

---
+++ src/br/com/forca/logica/Palavra.java
@@ -0,0 +1,20 @@
+package br.com.forca.logica;
+
+public class Palavra {
+	private String nome;
+	private String dica;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDica() {
+		return dica;
+	}
+	public void setDica(String dica) {
+		this.dica = dica;
+	}
+
+}
