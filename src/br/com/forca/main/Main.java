3d2284a679e88e4c2061aca8d347fba6efee63f4
jeferson <jefersongomes333@hotmail.com>
2020-08-14 14:36:54 -0300

    Create Main.java

---
+++ src/br/com/forca/main/Main.java
@@ -0,0 +1,14 @@
+package br.com.forca.main;
+
+import br.com.forca.logica.Forca;
+
+public class Main {
+	public static void main(String[] args) {
+				
+		Forca f = new Forca("jeferson");
+		f.newGame();
+		f.start();
+				 
+	}
+
+}
